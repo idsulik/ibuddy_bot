069c7a2725cce7c6dcc5a2c460a5d3e088322b3a
Suleiman Dibirov <idsulik@gmail.com>
2023-09-24 13:02:32 +0300

    fixed .env load

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -32,7 +32,7 @@ func main() {
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	telegramToken := os.Getenv(telegramTokenEnvName)
